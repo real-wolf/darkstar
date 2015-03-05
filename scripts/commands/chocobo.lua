d1d9b65d9d5a6f10a4be4e3586b17057a893d51b
real-wolf <mattias.the.medic@gmail.com>
2015-03-04 19:52:24 -0500

    cleaned up and replaced integers with enums
    replaced all the integer IDs with enums, it just looks nicer :3

--- scripts/commands/chocobo.lua
+++ scripts/commands/chocobo.lua
@@ -15,11 +15,11 @@ cmdprops =
 function onTrigger(player)
 	if (player:canUseChocobo() ==0) then
 		player:addStatusEffect(EFFECT_CHOCOBO, 1, 0, 99999); -- (effect, yes, something, time)
-		player:messagePublic(280, player, EFFECT_CHOCOBO, EFFECT_CHOCOBO);
-	elseif (player:canUseChocobo() ==316) then
-		player:messageBasic(316);
+		player:messagePublic(EFFECT_ENSTONE_II, player, EFFECT_CHOCOBO, EFFECT_CHOCOBO);
+	elseif (player:canUseChocobo() ==MSGBASIC_CANT_BE_USED_IN_AREA) then
+		player:messageBasic(MSGBASIC_CANT_BE_USED_IN_AREA);
 	elseif player:hasStatusEffect(EFFECT_CHOCOBO) then
-		player:messageBasic(523);
+		player:messageBasic(MSGBASIC_EFFECT_ALREADY_ACTIVE);
 	else
 		player:PrintToPlayer( "You require a chocobo license!" );
 	end
